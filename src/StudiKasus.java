a84664d0c2cbcf732b21ee636c180ec26503d74b
Jonathan Yudi <jonathanyudigun@gmail.com>
2019-09-23 18:29:15 +0700

    studi kasus siapin kerangka

--- src/StudiKasus.java
+++ src/StudiKasus.java
@@ -4,7 +4,6 @@ public class StudiKasus {
 	static Scanner input = new Scanner(System.in);
     static Matrix m = new Matrix(0, 0);
 
-   
     private static void menu(){
     	System.out.println("$ Pilihan Menu Studi Kasus $");
         System.out.println("0. Lihat matriks tersimpan");
@@ -19,30 +18,35 @@ public class StudiKasus {
         System.out.println("Apa yang ingin Anda lakukan?");
 
         int choice = input.nextInt();
+        char x;
         switch(choice){
             case 0: 
                 m.showMatrix();
                 break;
             case 1:
-            	
+            	x = input.next().charAt(0);
+                solve1(x); // splInv
                 break;
             case 2:
-            	// fungsi_di_sini
+            	x = input.next().charAt(0);
+                solve2(x); // splGauss
                 break;
             case 3:
-            	// fungsi_di_sini
+            	x = input.next().charAt(0);
+                solve3(x); // splGaussJordan
                 break;
             case 4:
-            	// fungsi_di_sini
+            	x = input.next().charAt(0);
+                solve4(x); // matriksKotak
                 break;
             case 5:
-            	// fungsi_di_sini
+                solve5(); // interpolasiRangkaian
                 break;
             case 6:
-            	// fungsi_di_sini
+                solve6(); // interpolasiPenduduk
                 break;
             case 7:
-            	// fungsi_di_sini
+                solve7(); // interpolasiFungsi
                 break;
             case 8:
             	break;
@@ -55,9 +59,60 @@ public class StudiKasus {
         else menu();
     }
 
+    private static void solve1(char x){
+        switch(x){
+            case 'a':
+
+            case 'b':
+
+            case 'c':
+
+            case 'd':
+
+        }
+    }
+
+    private static void solve2(char x){
+        switch(x){
+            case 'a':
+
+            case 'b':
+
+        }
+    }
+
+    private static void solve3(char x){
+        switch(x){
+            case 'a':
+
+            case 'b':
+
+        }
+    }
+
+    private static void solve4(char x){
+        switch(x){
+            case 'a': // 5x5, ada pilihan invers, deter, dll
+
+            case 'b': // 10x10
+
+        }
+    }
+
+    private static void solve5(){
+        // rangkaian
+    }
+
+    private static void solve6(){
+        // penduduk
+        int tahun = input.nextInt();
+    }
+
+    private static void solve7(){
+        int n = input.nextInt();  
+    }
+
     public static void main(String[] args){
         menu();
     }
-
- 
 }
